2b4a4cbe3f4323fa2068b1e94bfcfb09359f47a4
Aleksei Mialkin <aleksei@mialkin.ru>
2024-07-02 01:50:31 +0300

    Use Serilog

--- skeleton-api/src/Skeleton.Api/Program.cs
+++ skeleton-api/src/Skeleton.Api/Program.cs
@@ -1,15 +1,38 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+    configuration.WriteTo.Console();
+});
+
+var services = builder.Services;
+
+services.AddControllers();
+services.AddRouting(options => options.LowercaseUrls = true);
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(options =>
+{
+    options.DescribeAllParametersInCamelCase();
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Skeleton API", Version = "v1" });
+});
 
 var application = builder.Build();
 
-if (application.Environment.IsDevelopment())
+application.UseRouting();
+application.UseSerilogRequestLogging();
+
+application.UseSwagger();
+application.UseSwaggerUI(options =>
 {
-    application.UseSwagger();
-    application.UseSwaggerUI();
-}
+    options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "v1");
+    options.RoutePrefix = string.Empty;
+    options.DocumentTitle = "Skeleton API";
+});
 
 application.MapGet("/ping", () => "OK");
 
