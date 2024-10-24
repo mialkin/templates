d29d90604a8ffffc5907dbb03a47fbacdf40b9c5
Aleksei Mialkin <aleksei@mialkin.ru>
2024-10-24 22:43:42 +0300

    Add minimal-api template

---
+++ skeleton-minimal-api/readme.md
@@ -0,0 +1,11 @@
+# Skeleton
+
+## Prerequisites
+
+- [↑ .NET SDK 8](https://dotnet.microsoft.com/en-us/download/dotnet/8.0)
+
+## Running application
+
+```bash
+dotnet watch --project src/Skeleton.Api --no-hot-reload
+```
---
+++ skeleton-minimal-api/src/Skeleton.Api/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog(
+    (context, configuration) =>
+    {
+        configuration.ReadFrom.Configuration(context.Configuration);
+        configuration.WriteTo.Console();
+    });
+
+var services = builder.Services;
+
+services.AddControllers();
+services.AddRouting(options => options.LowercaseUrls = true);
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(
+    options =>
+    {
+        options.DescribeAllParametersInCamelCase();
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "Skeleton API", Version = "v1" });
+    });
+
+var application = builder.Build();
+
+application.UseRouting();
+application.UseSerilogRequestLogging();
+
+application.UseSwagger();
+application.UseSwaggerUI(
+    options =>
+    {
+        options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "v1");
+        options.RoutePrefix = string.Empty;
+        options.DocumentTitle = "Skeleton API";
+    });
+
+application.MapGet("/say-hello", () => "Hello world!");
+
+application.Run();
